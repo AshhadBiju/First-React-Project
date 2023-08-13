e113a905fd8a09d03a371b27e4cbe8f7dab6e877
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-08-13 03:53:23 -0600

    useEffect

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React from "react";
 
 function Counter() {
+  useEffect(() => {
+    console.log("Mounting...");
+    return () => {
+      console.log("Un-mounting...");
+    };
+  });
   const [count, setCount] = useState(0);
   return (
     <div>
