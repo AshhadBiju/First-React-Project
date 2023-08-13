b6078d223720616d8a8a6233a22df61b2f1ae8e3
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-08-13 03:06:08 -0600

    mount&un-mount

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1 onClick={() => setState(!state)}>Show/Hide</h1>{" "}
       {/*When we click this the setState function will work*/}
-      {state ? <Counter /> : null}
-    </div> // this is used to put the ternary operator of if statement. If state is true then Counter function will work or else null.
-  );
+      {state && <Counter />}
+    </div> // this is used to put the ternary operator of if statement. If state is true then Counter function will work or else null. && is used so that it works only if both stae and counter is true.
+  ); //when we click Show/Hide, the Hello I am Component is put, it's called mounting. Click again, it's gone, the component disappears, it's called un-mounting.
 }
 
 export default App;
