7bd04209f6c61ee7887e3c1fe09eb43aa1cb6912
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-08-14 15:56:03 -0600

    toDo-App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 import React from "react";
-import Counter from "./Counter";
-import { useState } from "react"; //first import the useState
+import { useState } from "react";
+import "./App.css";
+
 function App() {
-  const [state, setState] = useState(false); //this is how to define useState and input as false
+  const [toDos, setToDos] = useState([]); //we assign an empty array here, reason why is that
+  const [toDo, setToDo] = useState(""); //instaed of array, double quotes are used. Reason is that
   return (
-    <div className="App">
-      <h1 onClick={() => setState(!state)}>Show/Hide</h1>{" "}
-      {/*When we click this the setState function will work*/}
-      {state && <Counter />}
-    </div> // this is used to put the ternary operator of if statement. If state is true then Counter function will work or else null. && is used so that it works only if both stae and counter is true.
-  ); //when we click Show/Hide, the Hello I am Component is put, it's called mounting. Click again, it's gone, the component disappears, it's called un-mounting.
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, its Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i
+          onClick={() => setToDos([...toDos, toDo])}
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        <div className="todo">
+          <div className="left">
+            <input type="checkbox" name="" id="" />
+            <p>React tutorial</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
