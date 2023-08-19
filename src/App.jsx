471a3e4a7a33ebdead43518ae3fcec984744b1ae
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-08-19 07:45:47 -0600

    to-do

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   const [toDos, setToDos] = useState([]); //we assign an empty array here, reason why is that
-  const [toDo, setToDo] = useState(""); //instaed of array, double quotes are used. Reason is that
+  const [toDo, setToDo] = useState(""); //instead of array, double quotes are used. Reason is that
   return (
     <div className="app">
       <div className="mainHeading">
@@ -12,30 +12,71 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, its Wednesday 🌝 ☕ </h2>
+        <h2>Ummmm lamest to-do youll see</h2>
       </div>
       <div className="input">
         <input
           value={toDo}
-          onChange={(e) => setToDo(e.target.value)}
+          onChange={(e) => setToDo(e.target.value)} //this is to target the value
           type="text"
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() => setToDos([...toDos, toDo])}
+          onClick={
+            () =>
+              setToDos([
+                ...toDos,
+                { id: Date.now(), text: toDo, status: false },
+              ]) // here there's an id, already value set to string, here changed to text, status default is set to false
+          }
           className="fas fa-plus"
         ></i>
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {toDos.map((obj) => {
+          //then we map the whole thing into obj
+          return (
+            <div className="todo">
+              <div className="left">
+                <input
+                  onChange={(e) => {
+                    console.log(e.target.checked); //when the target value is checked in console log
+                    console.log(obj);
+                    setToDos(
+                      //using setToDos we filter the obj, also check if obj2 id is equal to obj id
+                      toDos.filter((obj2) => {
+                        if (obj2.id === obj.id) {
+                          obj2.status = e.target.checked;
+                        }
+                        return obj2;
+                      })
+                    );
+                  }}
+                  value={obj.status}
+                  type="checkbox"
+                  name=""
+                  id=""
+                />
+                <p>{obj.text}</p>
+              </div>
+              <div className="right">
+                <i className="fas fa-times"></i>
+              </div>
+            </div>
+          );
+        })}
+        <h3>Completed tasks</h3>
+        {toDos.map((obj) => {
+          //the completed tasks with the data is displayed ie, obj text
+          if (obj.status) {
+            return (
+              <div>
+                <h2>{obj.text}</h2>
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     </div>
   );
