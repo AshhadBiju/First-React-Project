2d1ac464b9397d2343c0bb985fa453af5611774c
Ashhad Biju <122079157+AshhadBiju@users.noreply.github.com>
2023-08-28 14:49:06 -0600

    Added a cover image, description & buttons.

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Banner.css";
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="content">
+        <h1 className="title">Movie Name</h1>
+        <div className="banner_button">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
+        </div>
+        <h1 className="description">
+          Enthekilum okke ang ittekk deyy, ith Netflix alle, veronnum allallo
+          ghfgfghfghfghfghfghgfhfghfghfghfghfghfghfgh
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
